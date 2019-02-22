e9c926295bfca4dde70b12ab24cbed52d35660f1
CK2800 <cph-ck83@cphbusiness.dk>
2019-02-22 12:59:28 +0100

    travis yml file addedW

--- src/main/java/dbfacades/DemoFacade.java
+++ src/main/java/dbfacades/DemoFacade.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /*
 Simple Facade demo for this start-up project
@@ -14,58 +15,118 @@ Simple Facade demo for this start-up project
   - Delete the three public methods below, and replace with your own Facade Logic 
   - Delete all content in the main method
 
-*/
-public class DemoFacade {
+ */
+public class DemoFacade
+{
 
-  EntityManagerFactory emf;
+    EntityManagerFactory emf;
 
-  public DemoFacade(EntityManagerFactory emf) {
-    this.emf = emf;
-  }
-
-  public Car addCar(Car car){
-    EntityManager em = emf.createEntityManager();
-    try{
-      em.getTransaction().begin();
-      em.persist(car);
-      em.getTransaction().commit();
-      return car;
-    }finally{
-      em.close();
-    }
-  }
-  
-  public List<Car> getAllCars() {
-    EntityManager em = emf.createEntityManager();
-    try {
-      return (List<Car>) em.createQuery("select m from Car m").getResultList();
-    } finally {
-      em.close();
-    }
-  }
-  public long countCars() {
-    EntityManager em = emf.createEntityManager();
-    try {
-      return (Long) em.createQuery("select Count(m) from Car m").getSingleResult();
-    } finally {
-      em.close();
+    public DemoFacade(EntityManagerFactory emf)
+    {
+        this.emf = emf;
     }
 
-  }
+    public Car addCar(Car car)
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            em.getTransaction().begin();
+            em.persist(car);
+            em.getTransaction().commit();
+            return car;
+        } finally
+        {
+            em.close();
+        }
+    }
 
-  /*
+    public List<Car> getAllCars()
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            return (List<Car>) em.createQuery("select m from Car m").getResultList();
+        } finally
+        {
+            em.close();
+        }
+
+    }
+
+    public long countCars()
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            return (Long) em.createQuery("select Count(m) from Car m").getSingleResult();
+        } finally
+        {
+            em.close();
+        }
+
+    }
+
+    /*
   This will only work when your have added a persistence.xml file in the folder: 
      src/main/resources/META-INF
   You can use the file: persistence_TEMPLATE.xml (in this folder) as your template
-  */
-  public static void main(String[] args) {
-    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
-    DemoFacade df = new DemoFacade(emf);
-    df.addCar(new Car("Volvo"));
-    df.addCar(new Car("WW"));
-    df.addCar(new Car("Jaguar"));
-    long numbOfCars = df.countCars();
-    System.out.println("Number of cars: "+numbOfCars);
-  }
+     */
+    public static void main(String[] args)
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+        DemoFacade df = new DemoFacade(emf);
+        df.addCar(new Car("Volvo"));
+        df.addCar(new Car("WW"));
+        df.addCar(new Car("Jaguar"));
+        long numbOfCars = df.countCars();
+        System.out.println("Number of cars: " + numbOfCars);
+    }
+
+    List<Car> getCarsByMake(String make)
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            Query q = em.createQuery("SELECT c FROM Car c WHERE c.make = :make").setParameter("make", make);
+            List<Car> result = q.getResultList();
+            return result;
+        }
+        finally
+        {
+            em.close();
+        }
+    }
+
+    Car getCarById(int i)
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            return em.find(Car.class, i);
+        } finally
+        {
+            em.close();
+        }
+    }
+
+    boolean deleteCar(Integer idToDelete)
+    {
+        EntityManager em = emf.createEntityManager();
+        
+        try
+        {            
+            em.remove(em.find(Car.class, idToDelete));
+            return true;
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        finally
+        {
+            em.close();
+        }
+    }
 
 }
--- src/test/java/dbfacades/FacadeTest.java
+++ src/test/java/dbfacades/FacadeTest.java
@@ -1,7 +1,7 @@
 package dbfacades;
 
-import dbfacades.DemoFacade;
 import entity.Car;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,51 +9,96 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
 /**
- * UNIT TEST example that mocks away the database with an in-memory database
- * See Persistence unit in persistence.xml in the test packages
- * 
- * Use this in your own project by:
- *   - Delete everything inside the setUp method, but first, observe how test data is created
- *   - Delete the single test method, and replace with your own tests
- * 
+ * UNIT TEST example that mocks away the database with an in-memory database See
+ * Persistence unit in persistence.xml in the test packages
+ *
+ * Use this in your own project by: - Delete everything inside the setUp method,
+ * but first, observe how test data is created - Delete the single test method,
+ * and replace with your own tests
+ *
  */
-public class FacadeTest {
-  
-EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-test", null);
+public class FacadeTest
+{
 
-DemoFacade facade = new DemoFacade(emf);
- 
-  /** Setup test data in the database to a known state BEFORE Each test */
-  @Before
-  public void setUp() {
-    EntityManager em = emf.createEntityManager();
-    try {
-      em.getTransaction().begin();
-      //Delete all, since some future test cases might add/change data
-      em.createQuery("delete from Car").executeUpdate();
-      //Add our test data
-      Car e1 = new Car("Volve");
-      Car e2 = new Car("WW");
-      em.persist(e1);
-      em.persist(e2);
-      em.getTransaction().commit();
-    } finally {
-      em.close();
-    }
-  }
+    // Test mod in-memory test database.
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu-test", null);
+    // Test mod lokal database.
+    //EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu", null);
 
-  // Test the single method in the Facade
-  @Test
-  public void countEntities() {
-    EntityManager em = emf.createEntityManager();
-    try {
-      long count = facade.countCars();
-      Assert.assertEquals(2,count);
-    } finally {
-      em.close();
+    DemoFacade facade = new DemoFacade(emf);
+    Car e1 = null, e2 = null;
+    
+    /**
+     * Setup test data in the database to a known state BEFORE Each test
+     */
+    @Before
+    public void setUp()
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            em.getTransaction().begin();
+            //Delete all, since some future test cases might add/change data
+            em.createQuery("delete from Car").executeUpdate();
+            //Add our test data
+            e1 = new Car("Volvo");
+            e2 = new Car("WW");
+            em.persist(e1);
+            em.persist(e2);
+            em.getTransaction().commit();
+        } finally
+        {
+            em.close();
+        }
     }
-  }
-  
+
+    // Test the single method in the Facade
+    @Test
+    public void countEntities()
+    {
+        EntityManager em = emf.createEntityManager();
+        try
+        {
+            long count = facade.countCars();
+            Assert.assertEquals(2, count);
+        } finally
+        {
+            em.close();
+        }
+    }
+
+    @Test
+    public void testGetAllCars()
+    {
+        List<Car> allCars = facade.getAllCars();
+        Assert.assertEquals(2, allCars.size());
+        //Assert.assertEquals("Volve", allCars.get(0).getMake()); // bad.
+
+    }
+
+    @Test
+    public void testGetCarsByMake()
+    {
+        List<Car> carsByMake = facade.getCarsByMake("Volvo");
+        Assert.assertEquals(1, carsByMake.size());
+    }
+
+    @Test
+    public void testGetCarById()
+    {
+        Integer idToFind = e1.getId();
+        Car carById = facade.getCarById(idToFind);
+        Assert.assertEquals(carById.getId(), idToFind);
+    }
+    
+    @Test
+    public void testDeleteCarById()
+    {
+        // Arrange        
+        Integer idToDelete = e1.getId();
+        boolean deleted = facade.deleteCar(idToDelete);
+        Assert.assertEquals(deleted, true);
+    }
+
 }
